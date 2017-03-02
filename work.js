9843db705f9305ace2b10462e47861755754ff1b
Tammy Hoskins <thoskins84@gmail.com>
2017-03-02 11:05:59 -0500

    first two answers

---
+++ work.js
@@ -0,0 +1,14 @@
+// answer #1
+var a = 7
+var c = 5
+console.log(a + c)
+// answer #1
+var t = "I'm"
+var s = "hungry"
+console.log(t + s)
+// answer #2
+var heels = ["manolo", "zanotti"];
+var sneakers = ["nike", "adidas"];
+var shoes = ["heels", "sneakers"];
+var shoes = [ ["manolo", "zanotti"], ["nike", "adidas"] ];
+console.log(shoes);
