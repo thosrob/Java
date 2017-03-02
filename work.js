2309459099cd10898d033f0b416548c1551bd19e
Tammy Hoskins <thoskins84@gmail.com>
2017-03-02 11:28:12 -0500

    homework

--- work.js
+++ work.js
@@ -12,3 +12,24 @@ var sneakers = ["nike", "adidas"];
 var shoes = ["heels", "sneakers"];
 var shoes = [ ["manolo", "zanotti"], ["nike", "adidas"] ];
 console.log(shoes);
+// answer #3
+x = 105
+if (x < 100){
+ console.log("this is less than 100");
+}else if (x > 100){
+  console.log("x is 105 and greater than 100")
+}
+// answer #4
+function me(Tammy){
+    console.log(me)
+  }
+// answer #5
+function game(door){
+    if (door===1){
+      console.log("dog");
+    }else if(door===2){
+      console.log("cat");
+    } else if(door===3){
+      console.log("lizard");
+    }
+  }
